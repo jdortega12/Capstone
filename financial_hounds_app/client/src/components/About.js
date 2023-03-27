edeca0294a53a8aea07f8203dfcbcf49d62095d6
Jaymin Ortega <jdortega@loyola.edu>
2023-03-27 15:59:44 -0400

    Front end about us and generic planner skeleton

--- financial_hounds_app/client/src/components/About.js
+++ financial_hounds_app/client/src/components/About.js
@@ -1,11 +1,22 @@
 import React from "react";
-
+import "../styles/About.css";
 
 const About = () => {
     return (
-      <>
-        <h1>Hello</h1>
-      </>
+      <div className="about">
+        <div>
+          <h1 className="headerAbout">About Us</h1>
+            <p className="paragraphAbout">
+            Greyhound Financial Planning aims to allow Loyola Students to plan out their monthly budget 
+            as well as provide other financial services including, other forms of financial planning, and investing services. 
+            </p>
+            <row className="myRow">
+               <button className="myButtonAbout"> Try Our Generic Budget Planner! </button>
+               <button className="myButtonAbout"> Sign Up for Retirement Planning! </button>
+               <button className="myButtonAbout"> Sign Up for Emergency Planning! </button>
+            </row>
+        </div>
+      </div>
     );
   }
   
